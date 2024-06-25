73dccb4f86bf75ede4abc50e166313bfe12df422
Ganga Ghosh <gangaghosh423@gmail.com>
2024-06-25 14:13:28 +0530

    Luz de la calle
    Luz de la calle
    
    Co-Authored-By: Picante <156997061+luzdelacalle@users.noreply.github.com>

---
+++ s7/js/open-table-widget.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($){if(typeof $.fn.selectpicker=='function'){$('.otw-selectpicker').selectpicker();}
+$('.otw-party-size-select').on('change',function(){$('.PartySize').val($(this).val());});var now=(Date.now()-86400000);$(".otw-reservation-date").datepicker({autohide:true,autopick:true,format:"mm/dd/yyyy",weekStart:0,filter:function(date){return date.valueOf()>=now;},template:('<div class="open-table-widget-datepicker datepicker-container">'+
+'<div class="datepicker-panel" data-view="years picker">'+
+'<ul>'+
+'<li data-view="years prev">&lsaquo;</li>'+
+'<li data-view="years current"></li>'+
+'<li data-view="years next">&rsaquo;</li>'+
+'</ul>'+
+'<ul data-view="years"></ul>'+
+'</div>'+
+'<div class="datepicker-panel" data-view="months picker">'+
+'<ul>'+
+'<li data-view="year prev">&lsaquo;</li>'+
+'<li data-view="year current"></li>'+
+'<li data-view="year next">&rsaquo;</li>'+
+'</ul>'+
+'<ul data-view="months"></ul>'+
+'</div>'+
+'<div class="datepicker-panel" data-view="days picker">'+
+'<ul>'+
+'<li data-view="month prev">&lsaquo;</li>'+
+'<li data-view="month current"></li>'+
+'<li data-view="month next">&rsaquo;</li>'+
+'</ul>'+
+'<ul data-view="week"></ul>'+
+'<ul data-view="days"></ul>'+
+'</div>'+
+'</div>')});$.fn.datepicker.noConflict();});
